c3edd582cc231aad28785c05b3c6d06932e90cde
krisnapramulyap <19560029@stmik.jayakarta.ac.id>
2022-09-17 03:16:24 +0700

    setup react app

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import Home from "./pages/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const root = document.getElementById("root");
+render(
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  </Provider>,
+  root
+)
