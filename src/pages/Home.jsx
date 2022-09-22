d08b797e1ec2edf90b2a8efc7e1c64d250a18c59
SadamWahidYotama <sadam.wahid10@gmail.com>
2022-09-22 19:31:32 +0700

    sementara

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import "../css/style.css"
 
-export default function Home() {
-    return (<div>
+function Home() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="green" variant="dark">
+      <Container>
+      <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/MediQ.png"
+              width="70"
+              height="38"
+              className="d-inline-block align-top"
+            />{' '}
+            
+          </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#Beranda">Beranda</Nav.Link>
+            <Nav.Link href="#BuatJanji">Buat Janji</Nav.Link>
+            <Nav.Link href="#TentangKami">Tentang Kami</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="Login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 
-<h1>123</h1>
-    </div >);
+  );
 }
+
+export default Home;
