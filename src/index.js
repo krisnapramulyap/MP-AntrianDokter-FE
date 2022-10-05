e2301d3801fd3751f1207341069c2174a6e1bf06
SadamWY <107306917+SadamWY@users.noreply.github.com>
2022-10-06 01:54:23 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
 import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import Home from "./pages/Home";
+import Janji from "./pages/Janji";
 import Register from "./pages/register";
 import Login from "./pages/Login";
 import Admin from "./pages/LoginAdmin";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const root = document.getElementById("root");
@@ -19,6 +21,7 @@ render(
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/buatjanji" element={<Janji />} />
       </Routes>
     </Router>
   </Provider>,
